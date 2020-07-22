198d580bb75dbe6368a69effd29f62219d8e9111
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-22 14:14:37 -0500

    styling and clean up before book 2

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -3,6 +3,7 @@ const journal = [
         id: 1,
         date: "07/24/2025",
         concept: "FlexBox",
+        author: "Samantha Maas",
         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
         mood: "Happy"
     },
@@ -10,6 +11,7 @@ const journal = [
         id: 2,
         date: "07/25/2025",
         concept: "HTML & CSS",
+        author: "Samantha Maas",
         entry: "We had the chance to practice our skills in our own Daily Journal.",
         mood: "Confident"
     },
@@ -17,6 +19,7 @@ const journal = [
         id: 3,
         date: "07/26/2025",
         concept: "HTML & CSS",
+        author: "Samantha Maas",
         entry: "I worked with a group on a group project. We had to create a travel brochure website. We practiced using HTML and CSS. We got a lot of experience in Git and GitHub.",
         mood: "Confident"
     },
@@ -24,6 +27,7 @@ const journal = [
         id: 4,
         date: "07/27/2025",
         concept: "Javascript",
+        author: "Samantha Maas",
         entry: "First day of Javascript. It was overwhelming and scary. I kind of felt like I understood but then when I had the chance to practice, I didn't quite get it. ",
         mood: "Stressed"
     }
